47d48638af96bdde164e961c887d054ae8a61759
sowluss <sowluss@gmail.com>
2018-07-15 00:08:41 +0300

    modified index.html, script.js, style.css of Laptoplace site

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Variables
-const items = document.querySelector('#stock-list');
+const items = document.querySelector('#stock-list'),
+      shoppingCartContent = document.querySelector('#cart-content tbody');
 
 
 
@@ -27,12 +28,46 @@ if(e.target.classList.contains('add-to-cart')) {
         const item = e.target.parentElement.parentElement;
 
         // read the values
-        getItemInfo(item);
+        getLaptopInfo(item);
     
         //  reads HTML information of the selected item
 
-        function getItemInfo(item) {
-            console.log(item);
+        function getLaptopInfo(item) {
+            //   create an object with item data
+            const laptopInfo = {
+                image: item.querySelector('img').src,
+                name: item.querySelector('h5').textContent,
+                price: item.querySelector('span').textContent,
+                id: item.querySelector('button').getAttribute('data-id')
+            }
+            // Insert into the shopping cart
+            addIntoCart(laptopInfo);
         }
+        // Display the selected laptop in the shopping cart
+
+        function addIntoCart(item) {
+            //  create a <tr>
+            const row = document.createElement('tr');
+
+            //  build a template 
+            row.innerHTML = `
+                <tr>
+                    <td>
+                        <img src="${item.image}" width=200>
+                    </td>
+                    <td>${item.name}</td>
+                    <td>${item.price}</td>
+                    <td>
+                    <a href="#" class="remove" data-id="${item.id}">X</a>
+                    </td>
+                </tr>
+            `;
+
+            // Add into the shopping cart
+            shoppingCartContent.appendChild(row);
+
+        }
+
+
 }
 }
