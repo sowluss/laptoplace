676b971f1cf5d2e1a018c80191e87d695f758079
sowluss <sowluss@gmail.com>
2018-07-15 22:42:53 +0300

    made changes in script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Variables
 const items = document.querySelector('#stock-list'),
-      shoppingCartContent = document.querySelector('#cart-content tbody');
+      shoppingCartContent = document.querySelector('#cart-content tbody'),
+      clearCartBtn = document.querySelector('#clear-cart');
 
 
 
@@ -14,6 +15,9 @@ function loadEventListeners()   {
 
     // when the remove button is clicked
     shoppingCartContent.addEventListener('click', removeItem);
+
+    // ClearCart Button
+    clearCartBtn.addEventListener('click', clearCart);
 }
 
  
@@ -68,7 +72,35 @@ if(e.target.classList.contains('add-to-cart')) {
 
             // Add into the shopping cart
             shoppingCartContent.appendChild(row);
+
+            // Add item to the storage
+            saveIntoStorage(item);
       }
+
+        // Add items to the local storage
+        function saveIntoStorage(item) {
+            let items = getItemsFromStorage();
+
+            //  Get the item into array
+            items.push(item);
+
+            // since storage only contains strings, we have to convert JSON into string
+            localStorage.setItem('items', JSON.stringify(items));
+        }
+
+        //   Get content from the storage
+        function getItemsFromStorage() {
+
+            let items;
+
+            // if something exists in the storage we get the value, otherwise create an empty array
+            if(localStorage.getItem('items') === null) {
+                items = [];
+            } else {
+                items = JSON.parse(localStorage.getItem('items'));
+            }
+            return(items);
+        }
 
 
 
@@ -78,4 +110,11 @@ function removeItem(e) {
     if(e.target.classList.contains('remove')) {
         e.target.parentElement.parentElement.remove();
     }
+}
+
+//  Clicking Cart Button clears shopping cart
+function clearCart() {
+        while(shoppingCartContent.firstChild) {
+            shoppingCartContent.removeChild(shoppingCartContent.firstChild);
+        }
 }
